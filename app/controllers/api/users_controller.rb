c94f5e0a9f2815cff93b8d617b3ebe49532206e9
Hunter Maddux <phmaddux@gmail.com>
2017-11-11 10:43:44 -0500

    User controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,28 @@
 class Api::UsersController < ApplicationController
+    def index
+        @users = Users.all
+        render json: @users
+    end
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+    def create
+        @user = User.create!(user_params)
+        render json: @user
+    end
+    def update
+        @user = User.find(params[:id])
+        @user.update!(user_params)
+        render json: @user
+    end
+    def destroy
+        @user = User.find(params[:id]).delete
+        render status: :ok
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :picture, :password, :email)
+    end
 end
