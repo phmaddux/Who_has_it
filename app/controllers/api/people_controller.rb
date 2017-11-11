1d55a4b762c5ef2a59b3fea5d95ce29d401a666b
Hunter Maddux <phmaddux@gmail.com>
2017-11-11 12:29:29 -0500

    people_controller

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,2 +1,32 @@
 class Api::PeopleController < ApplicationController
+    def index
+        @people = User.find(params[:user_id]).people.reverse
+        render json: @people
+    end
+    def show
+        @person = person.find(params[:id])
+        render json: @post
+    end
+    def create
+        @user = User.find(params[:user_id])
+        @person = Person.new(person_params)
+
+        @user.person << @person
+        @user.save!
+        render json @person
+    end
+    def update
+        @person = Person.find(params[:id])
+        @person.update!(person_params)
+        render json: @person
+    end
+    def destroy
+        @person = Person.find(params[:id]).delete
+        render status: :ok
+    end
+    private
+
+    def person_params
+        params.require(:person).permit(:nickname, :first_name, :last_name, :picture, :phone_number, :email, :would_lend_again, :notes)
+    end
 end
